5814ec0b8ee502a95ea5670fbb630dd950eb902b
Marcelo Filho <marcelo.leda@icomp.ufam.edu.br>
2022-12-06 16:34:08 -0400

    chore: adjust code for deploy on heroku

--- controllers/result.controller.js
+++ controllers/result.controller.js
@@ -9,18 +9,10 @@ export async function result(req, res, next){
             _id: ObjectId(id)
         })
         const {_id, title, expireAt} = poll
-        const findOptions = await optionsCollection.find({pollId: id}).toArray()
 
-        const teste = await votesCollection.aggregate([
-            {$match: {choiceId: {$in: [ObjectId("638f67a202508000bad581d8")]}}},
-            { $project: { quizMax: { $max: "$votes"} } }
-         ]).toArray()
+        const query = {createdAt: poll.expireAt} 
+        const findVotes =  await votesCollection.find(query).sort({"votes" : -1}).limit(1).toArray()
 
-
-        const query = {choiceId: findOptions._id} 
-        const findVotes =  await votesCollection.find().sort({"votes" : -1}).limit(1).toArray()
-        const countVotes = await votesCollection.find().toArray()
-        console.log("teste", teste)
         findVotes.map((id)=>{
             res.send({
                 _id,
